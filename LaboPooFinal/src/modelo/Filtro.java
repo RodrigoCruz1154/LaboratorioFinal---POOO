e878d422efe831705f211329c125dc285586c28f
LN710Q <LN710Q@DEILABO1-PC04>
2018-06-19 11:21:09 -0600

    Se creó la clase filtro

--- LaboPooFinal/src/modelo/Filtro.java
+++ LaboPooFinal/src/modelo/Filtro.java
@@ -10,5 +10,63 @@ package modelo;
  * @author LN710Q
  */
 public class Filtro {
+    private int num;
+    private int edad;
+    private String nombrePerro;
+    private String propietario;
+    private String raza;
+    private boolean estado;
+    
+    public Filtro(){}
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombrePerro() {
+        return nombrePerro;
+    }
+
+    public void setNombrePerro(String nombrePerro) {
+        this.nombrePerro = nombrePerro;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
     
 }
